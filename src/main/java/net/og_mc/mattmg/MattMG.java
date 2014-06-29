92ce826ab3a9eeb462351fc8ae184fdf041926dd
Ian Macalinao <ian@giza.us>
2014-06-29 16:16:09 -0500

    Rename main files to MattMG
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- src/main/java/net/og_mc/mattmg/MattMG.java
+++ src/main/java/net/og_mc/mattmg/MattMG.java
@@ -3,15 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package net.og_mc.mattkoth;
+package net.og_mc.mattmg;
 
+import net.og_mc.mattkoth.MattKOTH;
 import com.simplyian.cloudgame.CloudGame;
 
 /**
  *
  * @author ian
  */
-public class MattKOTHPlugin extends CloudGame {
+public class MattMG extends CloudGame {
 
     @Override
     public void addGameplays() {
